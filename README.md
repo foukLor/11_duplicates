4d4ced7b1783243ee5c70b4afcb904928b8a97dd
foukLor <vitaminotar@yandex.com>
2016-10-10 11:39:17 +0300

    add v0.1 of script that find duplicates by hash

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # 11_duplicates
+
+This script is using to find equal files.
+
+
+## Usage:
+
+```sh
+python duplicates.py path_of_directory
+
+```
+
+You can see the files, that unnecessary on your operating system.
+
+
+please use python v3.5
--- duplicates.py
+++ duplicates.py
@@ -1,8 +1,43 @@
+import os
+import sys
+import hashlib
 
+def get_all_files_in_dir(file_path):
+    all_files = []
+    for (root, dirs, files) in os.walk(file_path):
+        for file_in_dir in files:            
+            file_name_in_dir = os.path.join(root, file_in_dir)
+            all_files.append(file_name_in_dir)
+    return all_files
 
-def are_files_duplicates(file_path1, file_path_2):
-    pass
+def hash_of_file(file_path):
+    BLOCKSIZE = 65536
+    hasher = hashlib.md5()
+    with open(file_path, 'rb') as afile:
+        buf = afile.read(BLOCKSIZE)
+        while len(buf) > 0:
+            hasher.update(buf)
+            buf = afile.read(BLOCKSIZE)
+    return hasher.hexdigest()
+
+def is_contains_duplicates(files):
+    if files is None:
+        return
+    list_files = {}
+    dubl_files = []
+    for filename in files:
+        file_info = hash_of_file(filename)
+        if not file_info in list_files.keys():
+            list_files[file_info] = os.path.dirname(filename)
+        else:
+            dubl_files.append(filename)
+    return dubl_files
 
 
 if __name__ == '__main__':
-    pass
+    dir_name = sys.argv[1]
+    if not os.path.isdir(dir_name):
+        exit(1)
+    files = get_all_files_in_dir(dir_name)
+    duplicates = is_contains_duplicates(files)
+    print("These files should be delete to save the space\n {0}".format(duplicates))
