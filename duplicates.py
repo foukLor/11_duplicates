5d44b3ca2c10ea45f3b55a28ef77c2df57221a38
foukLor <vitaminotar@yandex.com>
2017-01-02 18:19:03 +0300

    fixed bug in file path and updated work with mutliple duplicates

--- duplicates.py
+++ duplicates.py
@@ -4,23 +4,24 @@ import sys
 
 def get_duplicates(dir_path):
     files_in_dir = {}
-    duplicates_files = []
+    duplicates_files = {}
     for (file_root, dirs, all_files) in os.walk(dir_path):
         for searching_file in all_files:
-            file_name = file_root + "/" + searching_file
-            file_key = searching_file + str(os.path.getsize(file_name))
+            file_name = os.path.join(file_root,searching_file)
+            file_key = searching_file +  str(os.path.getsize(file_name))
             path_of_file_in_dict = files_in_dir.get(file_key, False)
             if path_of_file_in_dict :
-                duplicates_files.append([path_of_file_in_dict, file_name])
+                duplicates_files.setdefault(path_of_file_in_dict,[]).append(file_name)
             else:
                 files_in_dir[file_key] = file_name
     return duplicates_files
 
 
 def print_duplicates(duplicates):
-    print("Those files are the same")
-    for duplicate in duplicates:
-        print("{0} {1}".format(duplicate[0], duplicate[1]))
+    print("These files should be deleted to save the space:")
+    for duplicate_list in duplicates.values():
+        for duplicate in duplicate_list:
+            print(duplicate)
 
 
 if __name__ == '__main__':
